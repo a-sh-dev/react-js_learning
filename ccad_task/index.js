f2eef944a0b73606c98c5992c565941f684ed02a
a-sh <iam.angie@ymail.com>
2021-10-11 12:23:09 +1100

    Initial commit

---
+++ ccad_task/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './Container/AppFunction';
+// import App from './Container/AppClass';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
