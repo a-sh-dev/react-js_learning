60bb994faf4456247247cd492825c66e9071f4a4
a-sh <iam.angie@ymail.com>
2021-10-22 15:58:48 +1100

    Finalise ccad_ravenous with fetch api yelp

--- ccad_ravenous/src/components/Business/Business.js
+++ ccad_ravenous/src/components/Business/Business.js
@@ -1,29 +1,41 @@
-import React from 'react'
-import './Business.css';
+import React from "react";
+import "./Business.css";
 
 class Business extends React.Component {
   render() {
-    const { name, state, zipCode, category, rating, reviewCount } = this.props.business;
+    const {
+      imageSrc,
+      name,
+      address,
+      city,
+      state,
+      zipCode,
+      category,
+      rating,
+      reviewCount
+    } = this.props.business;
     return (
-      <div className="Business">
-        <div className="image-container">
-          <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+      <div className='Business'>
+        <div className='image-container'>
+          <img src={imageSrc} alt='' />
         </div>
         <h2>{name}</h2>
-        <div className="Business-information">
-          <div className="Business-address">
-            <p>{name}</p>
-            <p>Bordertown</p>
-            <p>{state} {zipCode}</p>
+        <div className='Business-information'>
+          <div className='Business-address'>
+            <p>{address}</p>
+            <p>{city}</p>
+            <p>
+              {state} {zipCode}
+            </p>
           </div>
-          <div className="Business-reviews">
+          <div className='Business-reviews'>
             <h3>{category.toUpperCase()}</h3>
-            <h3 className="rating">{rating} stars</h3>
+            <h3 className='rating'>{rating} stars</h3>
             <p>{reviewCount} reviews</p>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
--- ccad_ravenous/src/components/BusinessList/BusinessList.js
+++ ccad_ravenous/src/components/BusinessList/BusinessList.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import './BusinessList.css';
-import Business from '../Business/Business';
+import React from "react";
+import "./BusinessList.css";
+import Business from "../Business/Business";
 
 class BusinessList extends React.Component {
   render() {
     return (
-      <div class="BusinessList">
-        {
-          this.props.businesses.map((business) => {
-            return <Business business={business} />;
-          })
-        }
+      <div class='BusinessList'>
+        {this.props.businesses.map((business) => {
+          return <Business business={business} key={business.id} />;
+        })}
       </div>
-    )
+    );
   }
 }
 
