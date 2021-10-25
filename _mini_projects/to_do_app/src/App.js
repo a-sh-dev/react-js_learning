8757dcbf8d0a6b1c189ad44bb5098eaaceb18f2c
a-sh <iam.angie@ymail.com>
2021-10-25 19:17:42 +1100

    Add miniproject ToDo App

---
+++ _mini_projects/to_do_app/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>To Do List</h1>
+      </header>
+      <p style={{ textAlign: "center" }}>Based on Dev Ed's Tutorial</p>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
+      <TodoList todos={todos} setTodos={setTodos} />
+    </div>
+  );
+}
+
+export default App;
