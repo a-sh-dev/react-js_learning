98d618a587bbbfa893eb1589146ad0de06dd52f8
a-sh <iam.angie@ymail.com>
2021-10-26 16:50:51 +1100

    JSmilga useEffect Tutorial

--- _freecodecamp/jsmilga_react_advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ _freecodecamp/jsmilga_react_advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
+
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    console.log("useEffect called");
+    if (value >= 1) {
+      document.title = `VALUE: ${value}`;
+    }
+  }, [value]);
+
+  console.log("component rendered");
+
+  return (
+    <>
+      <h3>useEffect Basics</h3>
+      <div style={{ marginTop: "3em" }}>
+        <h1>{value}</h1>
+        <button className="btn" onClick={() => setValue(value + 1)}>
+          Click Me
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default UseEffectBasics;
--- _freecodecamp/jsmilga_react_advanced/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ _freecodecamp/jsmilga_react_advanced/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    console.log("useEffect");
+    window.addEventListener("resize", checkSize);
+    return () => {
+      console.log("useEffect cleanup");
+      window.removeEventListener("resize", checkSize);
+    };
+  }, []);
+
+  console.log("RENDER");
+
+  return (
+    <>
+      <h3>useEffect cleanup</h3>
+      <div>
+        <h1>Window</h1>
+        <h2>{size} PX</h2>
+      </div>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
