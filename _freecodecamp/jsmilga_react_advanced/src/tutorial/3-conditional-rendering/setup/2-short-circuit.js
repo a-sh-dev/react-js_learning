f9acb5aa166129b7b06a06602d027799d7265892
a-sh <iam.angie@ymail.com>
2021-10-26 21:21:25 +1100

    JSmilga conditional rendering tutorial

--- _freecodecamp/jsmilga_react_advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ _freecodecamp/jsmilga_react_advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  // const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
+  // const firstValue = text || "hello world";
+  // const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      <h4>short circuit</h4>
+      {/* <div>
+        <h2>FirstValue: {firstValue}</h2>
+        <h2>SecondValue : {secondValue}</h2>
+      </div> */}
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        {isError ? "fix error" : "toggle error"}
+      </button>
+      <hr style={{ margin: "3em 0" }} />
+      <div>
+        {/* {isError && <h1>Error...</h1>} */}
+        {isError ? <h2>Ooops, There is an Error...</h2> : <h2>All Good!</h2>}
+      </div>
+    </>
+  );
 };
 
 export default ShortCircuit;
