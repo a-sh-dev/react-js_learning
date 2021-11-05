97b87544b9a08c03199a58b4a8704f2791e1deb3
a-sh <iam.angie@ymail.com>
2021-11-05 19:06:51 +1100

    JSmilga Grocery List tutorial

---
+++ _mini_projects/_jsmilgas/08_grocery_list/README.md
@@ -0,0 +1,20 @@
+# GROCERY LIST
+
+**REACT PRACTICE – MINI PROJECTS**
+
+![screenshot](./docs/ss_grocery_01.png)
+![screenshot](./docs/ss_grocery_02.png)
+![screenshot](./docs/ss_grocery_03.png)
+![screenshot](./docs/ss_grocery_04.png)
+
+**Learnings**
+
+- React Hooks:
+  - `useState` &  `useEffect` (`setTimeout`)
+  - adding, editing, removing items
+  - setting alert
+- Forms
+- Utilising `local storage`
+
+<small>Based on [*John Smilga's "Code 15 React Projects - Complete Course"*](https://youtu.be/a_7Z7C_JCyo) with FreeCodeCamp.org
+
---
+++ _mini_projects/_jsmilgas/08_grocery_list/src/App.js
@@ -0,0 +1,131 @@
+import React, { useState, useEffect } from "react";
+import List from "./components/List";
+import Alert from "./components/Alert";
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem("groceryList");
+  // check if the local storage key exists, add items to the list
+  if (list) {
+    return JSON.parse(localStorage.getItem("groceryList"));
+  } else {
+    // else create an empty array
+    return [];
+  }
+};
+
+function App() {
+  // Grocery list states
+  const [name, setName] = useState("");
+  const [list, setList] = useState(getLocalStorage());
+  // Handle edit item
+  const [editId, setEditId] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  // Modals
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: "",
+    type: ""
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      // display alert - can't be empty value
+      showAlert(true, "danger", "please enter value");
+    } else if (name && isEditing) {
+      // handle editing
+      setList(
+        list.map((item) => {
+          if (item.id === editId) {
+            return { ...item, title: name };
+          }
+          return item;
+        })
+      );
+      // clear input
+      setName("");
+      setEditId(null);
+      setIsEditing(false);
+      // show alert
+      showAlert(true, "success", "edit success");
+    } else {
+      // display alert
+      showAlert(true, "success", `${name} is added`);
+      // add new item
+      const newItem = {
+        id: new Date().getTime().toString(),
+        title: name
+      };
+      // update list array with newItem
+      setList([...list, newItem]);
+      // clear input field
+      setName("");
+    }
+  };
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+
+  const clearList = () => {
+    showAlert(true, "danger", "grocery list is cleared");
+    setList([]);
+  };
+
+  const removeItem = (id) => {
+    const removed = list.find((item) => item.id === id);
+    showAlert(true, "danger", `${removed.title} is removed`);
+    // display remaining items in the list
+    setList(list.filter((item) => item.id !== id));
+  };
+
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditId(id);
+    setName(specificItem.title);
+  };
+
+  // LOCAL STORAGE
+  useEffect(() => {
+    localStorage.setItem("groceryList", JSON.stringify(list));
+  }, [list]);
+
+  return (
+    <section className="section-center">
+      <form onSubmit={handleSubmit} className="grocery-form">
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+        <h3>
+          <span role="img" aria-label="bud" style={{ marginRight: "0.25em" }}>
+            🪴
+          </span>
+          grocery list
+        </h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g. eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button className="submit-btn" type="submit">
+            {isEditing ? "edit" : "add"}
+          </button>
+        </div>
+      </form>
+      {/* GROCERY LIST */}
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
+        </div>
+      )}
+    </section>
+  );
+}
+
+export default App;
