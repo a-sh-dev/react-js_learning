d99f960e121ee2c13dbac8de043ab5de17ab47c4
a-sh <iam.angie@ymail.com>
2021-11-06 15:02:14 +1100

    JSmilga custom hook useFetch with useCallback

--- _freecodecamp/jsmilga_react_advanced/src/App.js
+++ _freecodecamp/jsmilga_react_advanced/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Setup from "./tutorial/08-useContext/setup/1-context-api";
+import Setup from "./tutorial/09-custom-hooks/setup/1-fetch-example";
 
 function App() {
   return (
