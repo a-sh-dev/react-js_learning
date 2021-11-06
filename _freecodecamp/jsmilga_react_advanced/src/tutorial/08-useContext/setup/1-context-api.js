259727e0faab0ef85864f3ec7ab1948b0c721201
a-sh <iam.angie@ymail.com>
2021-11-06 14:40:10 +1100

    JSmilga simple useContext demo

--- _freecodecamp/jsmilga_react_advanced/src/tutorial/08-useContext/setup/1-context-api.js
+++ _freecodecamp/jsmilga_react_advanced/src/tutorial/08-useContext/setup/1-context-api.js
@@ -1,7 +1,12 @@
-import React, { useState, useContext } from 'react';
-import { data } from '../../../data';
+import React, { useState, useContext } from "react";
+import { data } from "../../../data";
+
 // more components
 // fix - context api, redux (for more complex cases)
+
+// Setup Context
+const PersonContext = React.createContext();
+// createContext() produces two components -> Provider, Consumer
 
 const ContextAPI = () => {
   const [people, setPeople] = useState(data);
@@ -10,33 +15,30 @@ const ContextAPI = () => {
       return people.filter((person) => person.id !== id);
     });
   };
+
   return (
-    <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
+    <PersonContext.Provider value={{ removePerson, people }}>
+      <h3>context API / useContext</h3>
+      <List />
+    </PersonContext.Provider>
   );
 };
 
-const List = ({ people, removePerson }) => {
+const List = () => {
+  const mainData = useContext(PersonContext);
   return (
     <>
-      {people.map((person) => {
-        return (
-          <SinglePerson
-            key={person.id}
-            {...person}
-            removePerson={removePerson}
-          />
-        );
+      {mainData.people.map((person) => {
+        return <SinglePerson key={person.id} {...person} />;
       })}
     </>
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+  const { removePerson } = useContext(PersonContext);
   return (
-    <div className='item'>
+    <div className="item">
       <h4>{name}</h4>
       <button onClick={() => removePerson(id)}>remove</button>
     </div>
