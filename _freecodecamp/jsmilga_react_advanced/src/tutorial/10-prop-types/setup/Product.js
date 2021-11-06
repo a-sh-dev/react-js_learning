074222116be5e20afb5e4f421166be599deaebde
a-sh <iam.angie@ymail.com>
2021-11-06 15:35:57 +1100

    JSmilga PropTypes tutorial

--- _freecodecamp/jsmilga_react_advanced/src/tutorial/10-prop-types/setup/Product.js
+++ _freecodecamp/jsmilga_react_advanced/src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import defaultImage from "../../../assets/default-image.jpeg";
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({ image, name, price }) => {
+  // if image property exist, use image.url
+  const imgUrl = image && image.url;
+
+  return (
+    <article className="product">
+      <img src={imgUrl || defaultImage} alt={name || "default image"} />
+      <h4>{name}</h4>
+      <p>${price || 99.99}</p>
+    </article>
+  );
 };
+
+// name of the component + propTypes
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired
+};
+
+// default property values to handle error
+// if not using short circuit operators
+// Product.defaultProps = {
+//   name: "product name",
+//   price: 99.99,
+//   image: defaultImage
+// };
 
 export default Product;
