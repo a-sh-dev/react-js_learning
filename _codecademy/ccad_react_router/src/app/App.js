9fe3acf9a707232e0ce5ed1b68c4afd02c10bb47
a-sh <iam.angie@ymail.com>
2021-12-17 15:13:26 +1100

    Configure initial Routes and NavLinks

--- _codecademy/ccad_react_router/src/app/App.js
+++ _codecademy/ccad_react_router/src/app/App.js
@@ -1,25 +1,33 @@
-import React from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import About from "../components/About";
-import SignUp from "../components/SignUp";
-import Articles from "../components/Articles";
-import Article from "../components/Article";
-import Categories from "../components/Categories";
-import Author from "../components/Author";
-import Profile from "../components/Profile";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import "./App.css";
+// Components
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import About from '../components/About';
+import SignUp from '../components/SignUp';
+import Articles from '../components/Articles';
+import Article from '../components/Article';
+import Categories from '../components/Categories';
+import Author from '../components/Author';
+import Profile from '../components/Profile';
+
+import './App.css';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-      <main>
-        {/* Add Routes here! */}
-      </main>
+      <Routes>
+        <Route path="/" />
+        <Route path="about" element={<About />} />
+        <Route path="sign-up" element={<SignUp />} />
+        <Route path="articles" element={<Articles />} />
+        <Route path="categories" element={<Categories />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
