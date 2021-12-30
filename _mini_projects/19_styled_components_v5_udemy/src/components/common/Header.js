8617d01d117c549166f81a8ccd140540efec2888
a-sh <iam.angie@ymail.com>
2021-12-30 22:17:45 +1100

    Add and stylise NavLink

--- _mini_projects/19_styled_components_v5_udemy/src/components/common/Header.js
+++ _mini_projects/19_styled_components_v5_udemy/src/components/common/Header.js
@@ -1,22 +1,45 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const HeaderWrapper = styled.header`
   height: 60px;
   width: 100%;
   box-sizing: border-box;
   display: flex;
-  padding: 16px;
+  padding: 0 16px;
   position: fixed;
   top: 0;
-  background: #eee;
+  background: rgb(0, 45, 79);
+`;
+
+const Menu = styled.nav`
+  display: flex;
+  position: relative;
+  border-bottom: none;
+  margin: auto 0 auto auto;
+  width: initial;
+  left: initial;
+  top: initial;
+`;
+
+const StyledLink = styled(NavLink)`
+  padding: 1rem;
+  display: block;
+  text-align: center;
+  margin: auto 0;
+  text-decoration: none;
+  text-transform: uppercase;
+  color: rgb(177, 255, 177);
+  letter-spacing: 1px;
 `;
 
 export default function Header() {
   return (
     <HeaderWrapper>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
+      <Menu>
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/login">Login</StyledLink>
+      </Menu>
     </HeaderWrapper>
   );
 }
